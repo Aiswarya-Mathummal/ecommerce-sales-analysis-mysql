c4a0663690ec73f5c4703d72409482717bff7e03
Aiswarya-Mathummal <93366761+Aiswarya-Mathummal@users.noreply.github.com>
2026-09-04 01:55:10 +0400

    create_tables.sql

---
+++ schema/create_tables.sql
@@ -0,0 +1,31 @@
+CREATE TABLE `customers` (
+    `customer_id` VARCHAR(20) NOT NULL,
+    `first_name` TEXT,
+    `last_name` TEXT,
+    `gender` TEXT,
+    `age_group` TEXT,
+    `signup_date` DATE DEFAULT NULL,
+    `country` TEXT,
+    PRIMARY KEY (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `orders` (
+    `order_id` VARCHAR(20) NOT NULL,
+    `customer_id` VARCHAR(20) DEFAULT NULL,
+    `order_date` DATE DEFAULT NULL,
+    `country` TEXT,
+    `category` TEXT,
+    `product` TEXT,
+    `quantity` INT DEFAULT NULL,
+    `unit_price` INT DEFAULT NULL,
+    `total_amount` INT DEFAULT NULL,
+    `order_status` TEXT,
+    `payment_method` TEXT,
+    `seller_id` VARCHAR(20) DEFAULT NULL,
+    PRIMARY KEY (`order_id`),
+    KEY `customer_id_idx` (`customer_id`),
+    CONSTRAINT `customer_id`
+        FOREIGN KEY (`customer_id`)
+        REFERENCES `customers` (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
